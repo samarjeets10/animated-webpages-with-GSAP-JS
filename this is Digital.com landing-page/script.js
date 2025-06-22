7ee3e532f083a4c7a1fef3c7cfed86aa644209c9
Samarjeet <samarsabale1021@gmail.com>
2025-06-22 23:31:05 +0530

    Add explore section with button and hover effects; enhance image transitions

---
+++ this is Digital.com landing-page/script.js
@@ -0,0 +1,23 @@
+
+gsap.from("#image-1", {
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    y: 60,
+    ease: "power2.out"
+})
+
+gsap.from("#image-2", {
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    x: 50,
+    ease: "power2.out"
+})
+gsap.from("#image-3", {
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    y: -50,
+    ease: "power2.out"
+})
